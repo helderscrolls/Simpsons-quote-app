9c87b54cf16cb7b6f544e288ed23fef8cf30a17f
Helder Nascimento <helder@lifeaz.fr>
2019-10-18 20:22:04 +0200

    update fetch to also display a single character on componentDidMount

--- src/components/display/DisplayRandom.js
+++ src/components/display/DisplayRandom.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React from 'react';
 
-const DisplayRandom = ({character}) => {
-  return (      
-  <div>
-    <img 
-      className="imgCharacter"
-      src={character.image} 
-      alt={character.name} 
-    />
-  <p>{character.name}</p>
-  </div>
-  )
+class DisplayRandom extends React.Component {
+  state = {
+    tabCharacter: '',
+  };
+
+  componentDidMount() {
+    this.handleClick();
+  }
+
+  random = max => {
+    return Math.floor(Math.random() * Math.floor(max));
+  };
+
+  handleClick = () => {
+    this.setState({ tabCharacter: this.state.characters[this.random(87)] });
+
+    console.log(this.state.characters);
+  };
+
+  render() {
+    if (this.props.isLoading) {
+      return <div>Loading…</div>;
+    }
+    return (
+      <div>
+        <img
+          className="imgCharacter"
+          src={this.props.characters.image}
+          alt={this.props.characters.name}
+        />
+        <p>{this.props.characters.name}</p>
+      </div>
+    );
+  }
 }
 
 export default DisplayRandom;
--- src/components/random/Random.js
+++ src/components/random/Random.js
@@ -1,75 +1,71 @@
-import React from 'react'
-import "./Random.css"
-
+import React from 'react';
+import './Random.css';
 
 class Random extends React.Component {
   state = {
-    characters: [],
-    isloaded: false,
-    tabCharacter: '',
-  }
+    charactersList: [],
+    isloading: true,
+    character: '',
+  };
 
-  getRandom = () => {
-    fetch("https://melroune.github.io/starwars-api/api/all.json")
-    .then(res => res.json()) 
-    .then(res => this.setState({characters: res, isloaded: true}))
+  fetchCharacters() {
+    fetch('https://melroune.github.io/starwars-api/api/all.json')
+      .then(res => res.json())
+      .then(res =>
+        this.setState({
+          charactersList: res,
+          character: res[this.random(87)],
+          isloading: false,
+        }),
+      );
   }
 
   componentDidMount() {
-    this.getRandom()
+    this.fetchCharacters();
   }
 
   random = max => {
     return Math.floor(Math.random() * Math.floor(max));
-  }
+  };
 
   handleClick = () => {
-    this.setState({tabCharacter : this.state.characters[this.random(87)]})
-  }
-  
-  render () {
+    this.setState({ character: this.state.charactersList[this.random(87)] });
+  };
+
+  render() {
     return (
-    <>
-    <ul>
-    <li>Name :{this.state.tabCharacter.name}</li>
-    <li>Height :{this.state.tabCharacter.height}</li>
-    <li>Mass :{this.state.tabCharacter.mass}</li>
-    <li>Gender :{this.state.tabCharacter.gender}</li>
-    <li>Homeworld :{this.state.tabCharacter.homeworld}</li>
-    <li>Wiki :{this.state.tabCharacter.wiki}</li>
-    <li><img 
-     className="imgCharacter" 
-     src={this.state.tabCharacter.image} 
-     alt= {this.state.tabCharacter.name}></img></li>
-    <li>Born :{this.state.tabCharacter.born}</li>
-    <li>Born Location :{this.state.tabCharacter.bornLocation}</li>
-    <li>Died :{this.state.tabCharacter.died}</li>
-    <li>Death Location :{this.state.tabCharacter.diedLocation}</li>
-    <li>Species :{this.state.tabCharacter.species}</li>
-    <li>Eye Color:{this.state.tabCharacter.eyeColor}</li>
-    <li>Hair Color :{this.state.tabCharacter.hairColor}</li>
-    <li>Skin Color:{this.state.tabCharacter.skinColor}</li>
-    <li>Cybernetics :{this.state.tabCharacter.cybernetics}</li>
-    </ul>
-    
-    <button 
-        className='NewCharacterButton'
-        onClick={this.handleClick}>
-      Get New Character
-    </button>
-    {/*  {!this.state.isloaded ? (<div>Loading...</div> 
-    ) : (
-     <div>
-       Random
-       <div>
-       {this.state.characters.map(character => (
-         <DisplayRandom character={character} key={character.id} />
-       ))}
-       </div>
-     </div>
-       )} */}
-    </>
-    )}
+      <>
+        <ul>
+          <li>Name :{this.state.character.name}</li>
+          <li>Height :{this.state.character.height}</li>
+          <li>Mass :{this.state.character.mass}</li>
+          <li>Gender :{this.state.character.gender}</li>
+          <li>Homeworld :{this.state.character.homeworld}</li>
+          <li>Wiki :{this.state.character.wiki}</li>
+          <li>
+            <img
+              className="imgCharacter"
+              src={this.state.character.image}
+              alt={this.state.character.name}
+            ></img>
+          </li>
+          <li>Born :{this.state.character.born}</li>
+          <li>Born Location :{this.state.character.bornLocation}</li>
+          <li>Died :{this.state.character.died}</li>
+          <li>Death Location :{this.state.character.diedLocation}</li>
+          <li>Species :{this.state.character.species}</li>
+          <li>Eye Color:{this.state.character.eyeColor}</li>
+          <li>Hair Color :{this.state.character.hairColor}</li>
+          <li>Skin Color:{this.state.character.skinColor}</li>
+          <li>Cybernetics :{this.state.character.cybernetics}</li>
+        </ul>
+
+        <button className="NewCharacterButton" onClick={this.handleClick}>
+          Get New Character
+        </button>
+      </>
+    );
+  }
 }
 
 export default Random;
